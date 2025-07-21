482636a00a1567bfc506bdd1a98e72d45481fea0
Shifatpreet <shifatpreetsingh@gmail.com>
2025-07-21 23:03:03 +0530

    Add files via upload

---
+++ Positive_Negative_Check.cpp.cpp
@@ -0,0 +1,34 @@
+//SHIFATPREET SINGH 
+//24070123098
+//ENTC B1
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the Number: "<<endl;
+    cin>>a;
+    if(a>0){
+        cout<<"The number you have entered is POSITIVE"<<endl;
+
+    }else if(a<0){
+        cout<<"The number you have entered is NEGATIVE "<<endl;
+
+    }else if(a==0){
+        cout<<"The number you have entered is ZERO"<<endl;
+
+    }else{
+        cout<<"INVALID INPUT"<<endl;
+    }
+    return 0;
+
+
+}
+/*Enter the Number: 
+89
+The number you have entered is POSITIVE
+Enter the Number: 
+-45
+The number you have entered is NEGATIVE 
+Enter the Number: 
+0
+The number you have entered is ZERO*/
